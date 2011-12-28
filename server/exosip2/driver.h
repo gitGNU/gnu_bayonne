f534dc10bce7fe80c980f74a6967f588d3ce935e
David Sugar <dyfet@gnutelephony.org>
2011-12-28 10:04:16 -0500

    Moved common static lib(s) to project root
    This will make it easier to have multiple local libs, which may happen.

--- server/exosip2/driver.h
+++ server/exosip2/driver.h
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <server.h>
+#include <common/server.h>
 #include <eXosip2/eXosip.h>
 #include <ucommon/secure.h>
 
