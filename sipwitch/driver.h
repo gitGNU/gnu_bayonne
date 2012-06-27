d4cf91cfcd90752e654d1c3f70151f32806c1b70
David Sugar <dyfet@gnutelephony.org>
2012-06-27 09:18:48 -0400

    fix for new arrangement

--- sipwitch/driver.h
+++ sipwitch/driver.h
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <common/server.h>
+#include <server/server.h>
 #include <ucommon/secure.h>
 
 #pragma GCC diagnostic ignored "-Wvariadic-macros"
